21b7987c43fe0a3422198cc9b608ccf0d15a69a0
Your Name <am5715073@gmail.com>
2024-04-18 00:25:23 +0200

    abouzaid

--- models/user.py
+++ models/user.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""
-Class User that inherits from BaseModel
-"""
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        __tablename__: Table name
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    Blueprint for a User object
-    Public Attributes that will use FileStorage in engine
-    folder to manage serialization and deserialization of User
-    """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", cascade="all, delete", backref="user")
+    reviews = relationship("Review", cascade="all, delete", backref="user")
